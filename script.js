2b6204317b11f07df27eb6452d5c8bc9a0c0bce2
RehanSaputra14 <hayyuhh2@gmail.com>
2026-01-27 09:51:09 +0700

    update db spreedsheet

--- script.js
+++ script.js
@@ -1,45 +1,28 @@
-// Inisialisasi EmailJS
-(function(){
-  emailjs.init("QtVREGDxgTJLDH1fj");
-})();
+const scriptURL = "https://script.google.com/macros/s/AKfycbyoagib6o8CG-OmUGs7BchmYSNdgPd_gbvUMk6-KjsR7hLpx-2msFYUJDfbfgqKN6CeZg/exec";
 
-document.getElementById("laporanForm").addEventListener("submit", function(e){
+document.getElementById("LaporanForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const nama = document.getElementById("nama").value;
-  const kelas = document.getElementById("kelas").value;
-  const judul = document.getElementById("judul").value;
-  const lokasi = document.getElementById("lokasi").value;
-  const isiLaporan = document.getElementById("deskripsi").value;
-
-  const laporan = {
-    nama,
-    kelas,
-    judul,
-    lokasi,
-    isiLaporan,
-    waktu: new Date().toLocaleString()
+  const data = {
+    nama: document.getElementById("nama").value,
+    kelas: document.getElementById("kelas").value,
+    judul: document.getElementById("judul").value,
+    kategori: document.getElementById("kategori").value,
+    lokasi: document.getElementById("lokasi").value,
+    deskripsi: document.getElementById("deskripsi").value
   };
 
-  // 🔹 SIMPAN KE LOCAL STORAGE
-  let data = JSON.parse(localStorage.getItem("riwayatLaporan")) || [];
-  data.push(laporan);
-  localStorage.setItem("riwayatLaporan", JSON.stringify(data));
-
-  // 🔹 KIRIM EMAIL
-  emailjs.send("service_g44qnpp", "template_df5bc1p", {
-    nama: nama,
-    kelas: kelas,
-    judul: judul,
-    lokasi: lokasi,
-    laporan: isiLaporan,
-    waktu: laporan.waktu
-  }).then(() => {
-    alert("Laporan berhasil dikirim!");
-    document.getElementById("laporanForm").reset();
-  }).catch((error) => {
-    alert("Gagal mengirim email");
-    console.log(error);
+  fetch(scriptURL, {
+    method: "POST",
+    body: JSON.stringify(data)
+  })
+  .then(res => res.json())
+  .then(() => {
+    alert("Laporan berhasil dikirim. Terima kasih 🙏");
+    document.getElementById("LaporanForm").reset();
+  })
+  .catch(err => {
+    alert("Gagal mengirim laporan!");
+    console.error(err);
   });
 });
-
