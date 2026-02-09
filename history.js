9fafe54f373c11f56d77acc8d2fccfe346da5f51
RehanSaputra14 <hayyuhh2@gmail.com>
2026-02-09 10:54:34 +0700

    update history.js

--- history.js
+++ history.js
@@ -2,6 +2,16 @@ const scriptURL = "https://script.google.com/macros/s/AKfycbyoagib6o8CG-OmUGs7Bc
 
 document.addEventListener("DOMContentLoaded", tampilkanHistory);
 
+function statusBadge(status) {
+  const s = String(status).toLowerCase();
+
+  if (s === "done") return "bg-success";
+  if (s === "pending") return "bg-warning";
+  if (s === "reject") return "bg-danger";
+
+  return "bg-secondary";
+}
+
 function tampilkanHistory() {
   const userId = localStorage.getItem("user_id");
   if (!userId) return;
@@ -24,7 +34,7 @@ function tampilkanHistory() {
         card.className = "col-12";
 
         card.innerHTML = `
-          <div class="p-4 rounded-4 shadow-sm bg-white">
+          <div class="p-2 rounded-4 shadow-sm bg-white w-100 m-auto">
         <!-- Header -->
         <div class="d-flex justify-content-between align-items-center mb-2">
           <div class="d-flex align-items-center gap-2">
@@ -32,8 +42,8 @@ function tampilkanHistory() {
             <small class="text-muted">${item.waktu}</small>
           </div>
 
-          <span class="badge rounded-pill bg-warning text-dark px-3 py-2">
-            <i class="bi bi-clock me-1"></i> ${item.status || "Pending"}
+          <span class="badge ${statusBadge(item.status)}">
+            ${item.status}
           </span>
         </div>
 
